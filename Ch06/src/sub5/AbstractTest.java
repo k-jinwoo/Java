c9e4f67a131d6f95e8c6c9d1310404017650bbcd
kim_3838 <kim_3838@naver.com>
2021-05-25 10:50:00 +0900

    2021/05/25 Java Ch06 수업예제

---
+++ Ch06/src/sub5/AbstractTest.java
@@ -0,0 +1,23 @@
+package sub5;
+
+/*
+ * 날짜 : 2021/05/25
+ * 이름 : 김진우
+ * 내용 : 추상클래스 실습하기
+ */
+public class AbstractTest {
+	public static void main(String[] args) {
+		// Unit unit = new Unit();
+		Unit marin = new Marin();
+		Unit zealot = new Zealot();
+		Unit zergling = new Zergling();
+		
+		marin.move();
+		zealot.move();
+		zergling.move();
+		
+		marin.attack();
+		zealot.attack();
+		zergling.attack();
+	}
+}
